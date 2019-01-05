dcb8f3b3b1eca22c4fd1939722393c26de926c3f
usefss <flasnm@yahoo.com>
2019-01-06 00:03:23 +0330

    Editing python code

--- python/run.py
+++ python/run.py
@@ -1,20 +1,21 @@
 from dijkstra import Node, dijkstra
 
-a = Node('a', 0)
-b = Node('b')
-d = Node('d')
-c = Node('c')
-e = Node('e')
-f = Node('f')
+if __name__=='__main__':
+        a = Node('a', 0)
+        b = Node('b')
+        d = Node('d')
+        c = Node('c')
+        e = Node('e')
+        f = Node('f')
 
-a.AddEdge(b ,10, f, 1)
-b.AddEdge(c,2, d, 3)
-c.AddEdge(e,6)
-d.AddEdge(e,1)
-e.AddEdge(f,1)
+        a.AddEdge(b ,10, f, 1)
+        b.AddEdge(c,2, d, 3)
+        c.AddEdge(e,6)
+        d.AddEdge(e,1)
+        e.AddEdge(f,1)
 
 
-dijkstra(a, '')
+        dijkstra(a, '')
 
-for node in Node.all:
-    node.__str__()
+        for node in Node.all:
+            node.__str__()
